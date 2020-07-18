4bf0af74638b2973a785dcbd9801467c7ee22396
yjkim1 <seaofnight@hanmail.net>
2020-07-18 13:09:20 +0900

    modify vimrc in spacevim

--- configure-vim-config.sh
+++ configure-vim-config.sh
@@ -2,9 +2,6 @@
 
 sudo apt-get install vim 
 
-#ln -s ${PWD}/vimrc ~/.vimrc
-
-
 # nerd font install 
 https://www.nerdfonts.com/font-downloads
 https://www.programmingfonts.org/
@@ -12,7 +9,13 @@ https://www.programmingfonts.org/
 ~/.fonts
 fc-cache -fv
 
-
 # install spacevim 
 
 curl -sLf https://spacevim.org/install.sh | bash
+
+# extra config 
+
+echo '
+set clipboard=unnamedplus
+set mouse=a
+' >> ~/.vim/vimrc 
